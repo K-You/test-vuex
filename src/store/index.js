5e38f185ff4b13e83ecbad14f539f118c0dfb296
Pierre BAUSIERE <pierre.bausiere@bell.ca>
2021-02-16 19:42:59 -0500

    <feat> init commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,43 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
+    counter: 0,
+    colorCode: 'blue',
   },
   mutations: {
+    increaseCounter(state, randomNumber) {
+      state.counter += randomNumber;
+    },
+    decreaseCounter(state, randomNumber) {
+      state.counter -= randomNumber;
+    },
+    setColorCode(state, newColor) {
+      state.colorCode = newColor;
+    }
   },
   actions: {
+    increaseCounter({ commit }) {
+      axios('https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new')
+        .then(response => {
+          commit('increaseCounter', response.data);
+        })
+    },
+    decreaseCounter({ commit }) {
+      axios('https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new')
+        .then(response => {
+          commit('decreaseCounter', response.data);
+        })
+    },
+    setColorCode({commit}, newColor) {
+      commit('setColorCode', newColor);
+    }
+  },
+  getters: {
+    counterSquared(state) {
+      return state.counter * state.counter;
+    }
   },
   modules: {
   }
